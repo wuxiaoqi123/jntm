e0fb70ad0b5a9290b4bdf459e16c15dfb41482d2
wuxiaoqi <1321972760@qq.com>
2021-04-26 14:30:19 +0800

    fragment_main

--- app/src/main/java/com/example/jntm/app/ext/CustomViewExt.kt
+++ app/src/main/java/com/example/jntm/app/ext/CustomViewExt.kt
@@ -2,6 +2,9 @@ package com.example.jntm.app.ext
 
 import android.app.Activity
 import android.view.inputmethod.InputMethodManager
+import com.example.jetpackmvvm.base.appContext
+import com.example.jntm.app.util.SettingUtil
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx
 
 fun hideSoftKeyboard(activity: Activity?) {
     activity?.let { act ->
@@ -14,4 +17,18 @@ fun hideSoftKeyboard(activity: Activity?) {
             )
         }
     }
+}
+
+fun BottomNavigationViewEx.init(navigationItemSelectedAction: (Int) -> Unit): BottomNavigationViewEx {
+    enableAnimation(true)
+    enableShiftingMode(false)
+    enableItemShiftingMode(true)
+    itemIconTintList = SettingUtil.getColorStateList(SettingUtil.getColor(appContext))
+    itemTextColor = SettingUtil.getColorStateList(appContext)
+    setTextSize(12F)
+    setOnNavigationItemSelectedListener {
+        navigationItemSelectedAction.invoke(it.itemId)
+        true
+    }
+    return this
 }
--- app/src/main/java/com/example/jntm/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/example/jntm/ui/fragment/MainFragment.kt
@@ -3,13 +3,20 @@ package com.example.jntm.ui.fragment
 import android.os.Bundle
 import com.example.jntm.R
 import com.example.jntm.app.base.BaseFragment
+import com.example.jntm.app.ext.init
 import com.example.jntm.databinding.FragmentMainBinding
 import com.example.jntm.viewmodel.state.MainViewModel
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : BaseFragment<MainViewModel, FragmentMainBinding>() {
 
     override fun layoutId() = R.layout.fragment_main
 
     override fun initView(savedInstanceState: Bundle?) {
+        mainBottom.init {
+            when (it) {
+
+            }
+        }
     }
 }
